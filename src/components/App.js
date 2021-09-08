0df75944155c9c0f93f9a99a9d7e33bf8ea894b7
maqsudinamdar <maqsudinamdar@gmail.com>
2021-09-08 11:17:11 +0530

    feat : add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from './Header';
+
 
 const App = () => {
     
     return (
-        <div className="ui container">
-            App
+        <div className="ui">
+            <Header />
         </div>
     );
 };
