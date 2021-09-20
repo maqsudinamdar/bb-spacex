122334a91221aad60dfbc5240bd225b5b3fd1ff8
maqsudinamdar <maqsudinamdar@gmail.com>
2021-09-20 18:07:28 +0530

    feat : implement date format

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,5 +1,16 @@
+import parseISO from "date-fns/parseISO";
+import format from "date-fns/format";
+
 
 export const getStatus = (upcoming, launchSuccess) => {
     const status = upcoming ? "upcoming" : launchSuccess ? "success" : "failed";
     return <span className={`round-span ${status}`}>{status}</span>;
 };
+
+
+export const formatDate = date => {
+    return format(
+        parseISO(date),
+        "dd MMMM yyyy 'at' HH:mm"
+    )
+}
